8ecaf50c1bccf1455b3464a757879d5b258e4425
Script Raccoon <scriptraccoon@gmail.com>
2024-01-25 22:20:01 +0100

    feature to delete recent docs

--- static/home.js
+++ static/home.js
@@ -1,3 +1,15 @@
+const clear_button = document.getElementById("clear_button")
+const recent_docs_display = document.getElementById("recent_docs")
+const recent_docs_list = recent_docs_display.querySelector("ul")
+
+init()
+
+function init() {
+	const recent_docs = get_saved_recent_docs()
+	display_recent_docs(recent_docs)
+	clear_button.addEventListener("click", clear_recent_docs)
+}
+
 function get_saved_recent_docs() {
 	try {
 		return JSON.parse(localStorage.getItem("recent_docs") ?? "[]")
@@ -7,17 +19,13 @@ function get_saved_recent_docs() {
 	}
 }
 
-function display_recent_docs() {
-	const recent_docs = get_saved_recent_docs()
+function display_recent_docs(recent_docs) {
 	if (recent_docs.length === 0) return
-	const recent_docs_display = document.querySelector(".recent_docs")
-	const recent_docs_list = recent_docs_display.querySelector("ul")
-	if (!recent_docs_display || !recent_docs_list) return
 	recent_docs_display.removeAttribute("hidden")
 	recent_docs_list.innerHTML = ""
 	recent_docs.forEach((doc) => {
-		const is_ok = typeof doc === "object" && "id" in doc && "title" in doc
-		if (!is_ok) return
+		const valid = typeof doc === "object" && "id" in doc && "title" in doc
+		if (!valid) return
 		const item = document.createElement("li")
 		const link = document.createElement("a")
 		link.href = `/document/${doc.id}`
@@ -28,4 +36,8 @@ function display_recent_docs() {
 	})
 }
 
-display_recent_docs()
+function clear_recent_docs() {
+	localStorage.removeItem("recent_docs")
+	recent_docs_display.setAttribute("hidden", "")
+	recent_docs_list.innerHTML = ""
+}
