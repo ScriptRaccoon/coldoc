1bb0b1207f4dbba214ea6c23dce84db16417f32f
Script Raccoon <scriptraccoon@gmail.com>
2024-01-26 01:15:05 +0100

    remove cookie parser as middleware

--- server/express.js
+++ server/express.js
@@ -2,13 +2,11 @@ import express from "express"
 import router from "./router.js"
 
 import { resolve_path } from "./utils.js"
-import cookie_parser from "cookie-parser"
 
 export function generate_server() {
 	const app = express()
 	const PORT = process.env.PORT || 3000
 
-	app.use(cookie_parser())
 	app.use(express.static(resolve_path("..", "static")))
 	app.set("view engine", "ejs")
 	app.set("views", resolve_path("..", "pages"))
